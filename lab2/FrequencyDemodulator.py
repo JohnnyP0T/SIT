2b19716a9e7a0fead69f2978052618476c7dcbb6
pokoynichekk <cckkcckk666888@gmail.com>
2022-10-04 23:36:34 +0700

    Add .gitignore And Fix CodeStyle a Little

--- lab2/FrequencyDemodulator.py
+++ lab2/FrequencyDemodulator.py
@@ -1,14 +1,10 @@
-import math
-import numpy
-
-
 def fd(s, K, N, Fd):
-    b = [[i for i in range(0, int(K/N))],
-             [i for i in range(0, int(K/N))]]
-    for i in range(0, int(K/N)):
+    b = [[i for i in range(0, int(K / N))],
+         [i for i in range(0, int(K / N))]]
+    for i in range(0, int(K / N)):
         t = 0
         state = 0
-        for k in range(N*i, N*(i+1)):
+        for k in range(N * i, N * (i + 1)):
             newState = getNewState(s[k], Fd)
 
             t = t + 1 if newState != 0 and (state == 0 or newState != state) else t
@@ -18,7 +14,7 @@ def fd(s, K, N, Fd):
     return b
 
 
-def getNewState(sk,Fd):
+def getNewState(sk, Fd):
     if sk > Fd:
         return 1
     elif sk < -Fd:
